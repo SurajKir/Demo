3a74b50459e88dea07d233f9e68120ba14c1d34e
Suraj Gehlot <suraj@mobiuso.com>
2019-09-20 17:43:24 +0530

    Bug fix where other characters were treated as ')'.

--- Parentheses.py
+++ Parentheses.py
@@ -12,12 +12,15 @@ flag = 1
 for i in string:
     if(i=='('):
         stack.append(i)
-    else:
+    elif(i==')'):
         if(len(stack)==0):
             flag = 0
             break
         else:
             stack.pop()
+    else:
+        flag = 0
+        break
         
 if(len(stack)==0 and flag!=0):
     print(True)
