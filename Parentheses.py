cc2d8bef2bd179a911fd1d440b49c138cbb93ca7
Suraj Gehlot <suraj@mobiuso.com>
2019-09-20 16:50:17 +0530

    Added the Parenthese.py file

---
+++ Parentheses.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 20 11:37:07 2019
+
+@author: newuser
+"""
+
+string = input()
+stack = []
+flag = 1
+for i in string:
+    if(i=='('):
+        stack.append(i)
+    else:
+        if(len(stack)==0):
+            flag = 0
+            break
+        else:
+            stack.pop()
+        
+if(len(stack)==0 and flag!=0):
+    print(True)
+else:
+    print(False)
