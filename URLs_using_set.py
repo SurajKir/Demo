a14d2c1ec366b34ac7ce80b9216db9d0eb858214
Suraj Gehlot <suraj@mobiuso.com>
2019-09-24 11:40:54 +0530

    Made the urlAdd function in the URLs_using_set program.

--- URLs_using_set.py
+++ URLs_using_set.py
@@ -5,20 +5,25 @@ Created on Mon Sep 23 15:30:55 2019
 
 @author: newuser
 """
+def urlAdd(set, url):
+    last_len = len(set)
+    set.add(url)
+    curr_len = len(set)
+    if(last_len == curr_len):
+        return False
+    else:
+        return True
+
 
 visited_URLs = set()
 flag = 1
 print('To exit enter "0"')
-last_len = len(visited_URLs)
 
 while(flag != 0):
     url = input()
     if(url == '0'):
         break
-    visited_URLs.add(url)
-    curr_len = len(visited_URLs)
-    if(curr_len == last_len):
-        print('Already Visited.')
-    else:
+    if(urlAdd(visited_URLs, url)):
         print('URL added to the set.')
-    last_len = len(visited_URLs)
+    else:
+        print('Already Visited')
