950ee1bb35c692bf0db3ab85a6413dc932708d41
Suraj Gehlot <suraj@mobiuso.com>
2019-09-24 11:49:52 +0530

    Use dictionary instead of lists in the URLs program.

--- URLs.py
+++ URLs.py
@@ -5,21 +5,27 @@ Created on Mon Sep 23 15:18:38 2019
 
 @author: newuser
 """
+def urlAdd(dict, url):
+    hashed_url = hash(url)
+    if(hashed_url in dict.keys()):
+        return False
+    else:
+        dict[hashed_url] = url
+        return True
 
 
-visited_URLs = []
+visited_URLs = {}
 flag = 1
 print('To exit enter "0"')
 
 while(flag != 0):
     url = input()
-    if(url in visited_URLs):
-        print('Already visited.')
-    elif(url == '0'):
+    if(url == '0'):
         flag = 0
         break
-    else:
-        visited_URLs.append(url)
+    if(urlAdd(visited_URLs, url)):
         print('URL added.')
+    else:
+        print('Already visited.')
 
 
